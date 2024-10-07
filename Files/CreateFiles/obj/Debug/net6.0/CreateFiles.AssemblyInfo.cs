49c0cae7e060d4b98f559c5b7fc6af08c9535c42
Xenoraa <xennyokonti@gmail.com>
2024-10-07 10:37:04 +0100

    Searching

--- Files/CreateFiles/obj/Debug/net6.0/CreateFiles.AssemblyInfo.cs
+++ Files/CreateFiles/obj/Debug/net6.0/CreateFiles.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CreateFiles")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef3cc504fd4cb2707b0b5abd35fafa94d28b7985")]
 [assembly: System.Reflection.AssemblyProductAttribute("CreateFiles")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CreateFiles")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Files/FileInfo/obj/Debug/net6.0/FileInfo.AssemblyInfo.cs
+++ Files/FileInfo/obj/Debug/net6.0/FileInfo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileInfo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef3cc504fd4cb2707b0b5abd35fafa94d28b7985")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileInfo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileInfo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
