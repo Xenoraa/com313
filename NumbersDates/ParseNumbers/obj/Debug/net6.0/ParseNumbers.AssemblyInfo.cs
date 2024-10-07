9432841fba11254f129c8266476b2c571016fe30
Xenoraa <xennyokonti@gmail.com>
2024-10-07 11:19:26 +0100

    first commit

--- NumbersDates/ParseNumbers/obj/Debug/net6.0/ParseNumbers.AssemblyInfo.cs
+++ NumbersDates/ParseNumbers/obj/Debug/net6.0/ParseNumbers.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ParseNumbers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49c0cae7e060d4b98f559c5b7fc6af08c9535c42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+680480b17295d7b2ca980f5dbcfc2341ff436309")]
 [assembly: System.Reflection.AssemblyProductAttribute("ParseNumbers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ParseNumbers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
